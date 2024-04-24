8fa0c8dbca354896b683bb09f5c6aaac555dcf32
shaiadul <devshaiadul69@gmail.com>
2024-04-24 11:47:02 +0600

    fixed padding for desktop is 20

--- components/global/TobLocationBar.jsx
+++ components/global/TobLocationBar.jsx
@@ -8,7 +8,7 @@ export default function TopLocationBar() {
     { name: "Cart", url: "/" },
   ];
   return (
-    <section className="text-[#3E445A] text-md container px-5 py-3 hidden md:grid grid-cols-2 justify-between items-center border shadow-sm">
+    <section className="text-[#3E445A] text-md container px-5 md:px-20 py-3 hidden md:grid grid-cols-2 justify-between items-center border shadow-sm">
       <div>
         <ul className="flex justify-start items-center gap-5">
           {linkData.map((link, index) => (
