a0fe1b88399c60e846eb4ad6353b836d8db5fd86
shaiadul <devshaiadul69@gmail.com>
2024-04-29 10:34:18 +0600

    min max w

--- components/global/TobLocationBar.jsx
+++ components/global/TobLocationBar.jsx
@@ -10,7 +10,7 @@ export default function TopLocationBar() {
     { name: "Cart", url: "/" },
   ];
   return (
-    <section className="text-[#3E445A] text-sm px-5 md:px-44 lg:px-40 py-3 hidden md:grid grid-cols-2 justify-stretch items-center border shadow-sm">
+    <section className="text-[#3E445A] text-sm px-5 md:px-44 py-3 hidden min-w-full md:grid grid-cols-2 justify-stretch items-center border shadow-sm">
       <div>
         <ul className="flex justify-start items-center gap-5">
           {linkData.map((link, index) => (
