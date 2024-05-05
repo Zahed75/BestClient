5010edff092c8a69341c44c3c3d98a4dbf4c45f9
shaiadul <devshaiadul69@gmail.com>
2024-05-05 15:59:37 +0600

    menu basic

--- components/global/NavigationBar.jsx
+++ components/global/NavigationBar.jsx
@@ -1,10 +1,58 @@
+"use client";
 import arrayBottom from "@/public/images/arrayBottom.svg";
+import arrayBottomBlack from "@/public/images/arrayBottomBlack.svg";
 import treeDot from "@/public/images/treeDot.svg";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function NavigationBar() {
+  const [isBrowseProducts, setIsBrowseProducts] = useState(false);
+  const [isElectronicsSubMenuOpen, setIsElectronicsSubMenuOpen] =
+    useState(false);
+  const [isSmartSubMenuOpen, setIsSmartSubMenuOpen] = useState(false);
+
+  const handleBrowseProducts = () => {
+    setIsBrowseProducts(!isBrowseProducts);
+  };
+
+  const browseMenusItems = [
+    {
+      title: "Electronics",
+      link: "/",
+      subMenu: [
+        {
+          title: "Laptops",
+          link: "/",
+        },
+        {
+          title: "Smartphones",
+          link: "/",
+          subMenu: [
+            {
+              title: "Samsung",
+              link: "/",
+            },
+            {
+              title: "Apple",
+              link: "/",
+            },
+            {
+              title: "Xiaomi",
+              link: "/",
+            },
+          ],
+        },
+        {
+          title: "Cameras",
+          link: "/",
+        },
+      ],
+    },
+  ];
+
   return (
-    <nav className="px-5 md:px-44 min-w-full md:grid grid-cols-2 justify-stretch items-center">
+    <nav className="px-5 md:px-44 min-w-full md:flex justify-stretch items-center">
       <div className="relative">
         <button className="bg-[#F16521] text-sm font-normal px-5 py-4 uppercase text-white rounded-full flex justify-start items-center relative">
           <Image src={treeDot} className="w-4 h-4 mr-5" alt="Tree Dot" /> All
@@ -19,7 +67,125 @@ export default function NavigationBar() {
           </span>
         </button>
       </div>
-      <div></div>
+
+      <div className="hidden md:flex ml-auto">
+        <div className="flex justify-end items-center uppercase text-sm gap-x-1">
+          <span className="bg-[#F0FAFF] text-[#F16521] px-3 py-2 rounded-full cursor-pointer">
+            <Link href="/">Home</Link>
+          </span>
+          <div className="group relative">
+            <button
+              onClick={handleBrowseProducts}
+              className="flex justify-start items-center group-hover:bg-[#F0FAFF] group-hover:text-[#F16521] px-3 py-2 rounded-full cursor-pointer duration-700 uppercase"
+            >
+              Browse Products
+              <Image
+                src={arrayBottomBlack}
+                className="w-4 h-4 ml-5 -rotate-90 group-hover:rotate-1 duration-700"
+                alt="Array Bottom"
+              />
+            </button>
+            <div
+              className={`${
+                isBrowseProducts ? "block" : "hidden"
+              } absolute left-0 mt-2 w-64 bg-white rounded-lg border border-gray-200 shadow-xl`}
+            >
+              <ul className="text-sm">
+                <li className="py-2 px-3 hover:text-[#F16521] cursor-pointer">
+                  Home Appliance & Furniture
+                </li>
+                <li
+                  className="py-2 px-3 relative"
+                  onMouseEnter={() => setIsElectronicsSubMenuOpen(true)}
+                  onMouseLeave={() => setIsElectronicsSubMenuOpen(false)}
+                >
+                  <span className="flex items-center justify-between w-full hover:text-[#F16521] cursor-pointer">
+                    <span>Electronics</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`h-4 w-4 transition-transform duration-300 transform ${
+                        isElectronicsSubMenuOpen ? "-rotate-90" : "rotate-0"
+                      }`}
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M6.293 7.707a1 1 0 0 1 1.414 0L10 10.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
+                      />
+                    </svg>
+                  </span>
+
+                  {isElectronicsSubMenuOpen && (
+                    <ul className="absolute left-full top-0 mt-0 ml-1 p-3 w-48 bg-white rounded-lg border border-gray-200 shadow-xl">
+                      <li className="py-2 hover:text-[#F16521] cursor-pointer">
+                        Laptops
+                      </li>
+                      <li
+                        onMouseEnter={() => setIsSmartSubMenuOpen(true)}
+                        onMouseLeave={() => setIsSmartSubMenuOpen(false)}
+                        className="py-2 cursor-pointer relative"
+                      >
+                        <span className="flex items-center justify-between w-full hover:text-[#F16521] cursor-pointer">
+                          <span>Smartphone</span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className={`h-4 w-4 transition-transform duration-300 transform ${
+                              isSmartSubMenuOpen ? "-rotate-90" : "rotate-0"
+                            }`}
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M6.293 7.707a1 1 0 0 1 1.414 0L10 10.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
+                            />
+                          </svg>
+                        </span>
+
+                        {isSmartSubMenuOpen && (
+                          <ul className="absolute left-full top-0 mt-0 ml-4 p-3 w-48 bg-white rounded-lg border border-gray-200 shadow-xl">
+                            <li className="py-2 hover:text-[#F16521] cursor-pointer">
+                              I Phone
+                            </li>
+                            <li className="py-2 hover:text-[#F16521] cursor-pointer">
+                              Samsung
+                            </li>
+                            <li className="py-2 hover:text-[#F16521] cursor-pointer">
+                              Xiaomi
+                            </li>
+                          </ul>
+                        )}
+                      </li>
+                      <li className="py-2 hover:text-[#F16521] cursor-pointer">
+                        Cameras
+                      </li>
+                    </ul>
+                  )}
+                </li>
+                <li className="py-2 px-3 hover:text-[#F16521] cursor-pointer">
+                  Mobile & Accessories
+                </li>
+              </ul>
+            </div>
+          </div>
+          <span className="hover:bg-[#F0FAFF] hover:text-[#F16521] px-3 py-2 rounded-full cursor-pointer duration-700">
+            <Link href="/">All Products</Link>
+          </span>
+          <span className="hover:bg-[#F0FAFF] hover:text-[#F16521] px-3 py-2 rounded-full cursor-pointer duration-700">
+            <Link href="/">Quick emi</Link>
+          </span>
+          <span className="hover:bg-[#F0FAFF] hover:text-[#F16521] px-3 py-2 rounded-full cursor-pointer duration-700">
+            <Link href="/">super kisti</Link>
+          </span>
+          <span className="hover:bg-[#F0FAFF] hover:text-[#F16521] px-3 py-2 rounded-full cursor-pointer duration-700">
+            <Link href="/">your order</Link>
+          </span>
+          <span className="hover:bg-[#F0FAFF] hover:text-[#F16521] px-3 py-2 rounded-full cursor-pointer duration-700">
+            <Link href="/">contact us</Link>
+          </span>
+        </div>
+      </div>
     </nav>
   );
 }
