3103f4006d35ebe5f61d680b883bca50580c4471
shaiadul <devshaiadul69@gmail.com>
2024-05-20 11:40:23 +0600

    view more button adding

--- components/productCard/EventProduct.jsx
+++ components/productCard/EventProduct.jsx
@@ -4,12 +4,12 @@ import ProductCard from "./ProductCard";
 export default function EventProduct({ event }) {
   return (
     <main className="my-5 md:my-10">
-      <section className="grid grid-cols-2 justify-between items-center">
+      <section className="flex justify-between items-center gap-1">
         <div>
-          <h4 className="text-[#202435] text-xl font-semibold uppercase">
+          <h4 className="text-[#202435] text-md md:text-xl font-semibold uppercase">
             {event}
           </h4>
-          <h4 className="text-[#9B9BB4] text-sm font-semibold">
+          <h4 className="text-[#9B9BB4] text-xs md:text-sm font-semibold">
             Do not miss our best selling products from Washing Machine category.
           </h4>
         </div>
