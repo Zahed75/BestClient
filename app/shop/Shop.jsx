b4fc490e1e41f963903c46e1a2c486d52824356f
shaiadul <devshaiadul69@gmail.com>
2024-06-25 12:55:05 +0600

    fecth use for ssr

--- app/shop/Shop.jsx
+++ app/shop/Shop.jsx
@@ -5,10 +5,11 @@ import { TiThMenu } from "react-icons/ti";
 import { RiGridFill } from "react-icons/ri";
 import { useState } from "react";
 import ShopCard from "@/components/productCard/shopCard";
+import Skeleton from "@/components/global/Skeleton";
 
-export default function Shop({products}) {
+export default function Shop({ products }) {
   // const products =  fetchApi("/product/getAllProducts", "GET");
-  // console.log(products);
+  console.log("ssr product: ", products);
 
   const tagValues = ["Home", "Shop"];
   const brands = [
@@ -73,145 +74,149 @@ export default function Shop({products}) {
 
   return (
     <main className="container">
-      <div className="my-10">
-        <TagLine tagValues={tagValues} />
-        <section className="grid grid-cols-1 md:grid-cols-6 justify-between items-start gap-5">
-          <div>
+      {products ? (
+        <div className="my-10">
+          <TagLine tagValues={tagValues} />
+          <section className="grid grid-cols-1 md:grid-cols-6 justify-between items-start gap-5">
             <div>
-              <h1 className="uppercase">FILTER BY CATEGORIES</h1>
-              <div className="text-sm my-5">
-                <div className="flex justify-start items-center gap-3 py-1">
-                  <input
-                    id="all"
-                    type="checkbox"
-                    className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:border-gray-600"
-                  />
-                  <label htmlFor="all">All Categories</label>
+              <div>
+                <h1 className="uppercase">FILTER BY CATEGORIES</h1>
+                <div className="text-sm my-5">
+                  <div className="flex justify-start items-center gap-3 py-1">
+                    <input
+                      id="all"
+                      type="checkbox"
+                      className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:border-gray-600"
+                    />
+                    <label htmlFor="all">All Categories</label>
+                  </div>
+                  <div className="flex justify-start items-center gap-3 py-1">
+                    <input
+                      id="brand"
+                      type="checkbox"
+                      className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:border-gray-600"
+                    />
+                    <label htmlFor="brand">All Brand</label>
+                  </div>
+                  <div className="flex justify-start items-center gap-3 py-1">
+                    <input
+                      id="conion"
+                      type="checkbox"
+                      className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:border-gray-600"
+                    />
+                    <label htmlFor="conion">Conion</label>
+                  </div>
                 </div>
-                <div className="flex justify-start items-center gap-3 py-1">
-                  <input
-                    id="brand"
-                    type="checkbox"
-                    className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:border-gray-600"
-                  />
-                  <label htmlFor="brand">All Brand</label>
+              </div>
+              <div className="my-10">
+                <h1 className="uppercase">Filter by Price</h1>
+                <div className="text-sm my-5">
+                  <div>
+                    <input
+                      type="range"
+                      id="rangeInput"
+                      name="rangeInput"
+                      min="0"
+                      max="100"
+                    />
+                  </div>
+                  <div className="mt-3">
+                    <label htmlFor="rangeInput">Price: ৳ 550 — ৳ 543,200</label>
+                  </div>
                 </div>
-                <div className="flex justify-start items-center gap-3 py-1">
-                  <input
-                    id="conion"
-                    type="checkbox"
-                    className="w-4 h-4 bg-gray-100 rounded border-gray-300   dark:border-gray-600"
-                  />
-                  <label htmlFor="conion">Conion</label>
+              </div>
+              <div>
+                <h1 className="uppercase">FILTER BY Brand</h1>
+                <div className="text-sm my-5 h-60 overflow-y-scroll scrollbar_hidden cursor-all-scroll">
+                  {brands.map((brand, index) => (
+                    <button
+                      key={index}
+                      className="flex justify-between items-center gap-3 py-1"
+                    >
+                      {brand.name}
+                      <p>({brand.count})</p>
+                    </button>
+                  ))}
                 </div>
               </div>
             </div>
-            <div className="my-10">
-              <h1 className="uppercase">Filter by Price</h1>
-              <div className="text-sm my-5">
-                <div>
-                  <input
-                    type="range"
-                    id="rangeInput"
-                    name="rangeInput"
-                    min="0"
-                    max="100"
+            <div className="md:col-span-5">
+              <div className="bg-[#f7f8fd] py-4 px-5 w-full rounded-md flex justify-between items-center">
+                <div className="flex justify-start items-center gap-5">
+                  <TiThMenu
+                    onClick={() => handleDynamicGrid({ value: 1 })}
+                    className={`text-2xl text-gray-400 hover:text-gray-700 ${
+                      dynamicGrid == 1 ? "text-gray-700" : ""
+                    } duration-700`}
+                  />
+
+                  <RiGridFill
+                    onClick={() => handleDynamicGrid({ value: 3 })}
+                    className={`text-2xl text-gray-400 hover:text-gray-700 ${
+                      dynamicGrid == 3 ? "text-gray-700" : ""
+                    } duration-700`}
+                  />
+                  <TfiLayoutGrid4Alt
+                    onClick={() => handleDynamicGrid({ value: 4 })}
+                    className={`text-xl text-gray-400 hover:text-gray-700 ${
+                      dynamicGrid == 4 ? "text-gray-700" : ""
+                    } duration-700`}
                   />
                 </div>
-                <div className="mt-3">
-                  <label htmlFor="rangeInput">Price: ৳ 550 — ৳ 543,200</label>
+                <div className="flex items-center gap-3 ">
+                  <div>
+                    <select
+                      name="sort"
+                      id="sort"
+                      className="focus:ring-0 focus:outline-0 px-3 "
+                    >
+                      <option className="" value="default">
+                        Short by latest
+                      </option>
+                      <option className="" value="pricehtl">
+                        Price: high to low
+                      </option>
+                      <option className="" value="pricelth">
+                        Price: low to high
+                      </option>
+                    </select>
+                  </div>
+                  <div>
+                    show
+                    <select
+                      name="show"
+                      id="show"
+                      className="focus:ring-0 focus:outline-0 px-3 border-none"
+                    >
+                      <option className="" value="default">
+                        9
+                      </option>
+                      <option className="" value="18">
+                        18
+                      </option>
+                      <option className="" value="27">
+                        27
+                      </option>
+                      <option className="" value="36">
+                        36
+                      </option>
+                    </select>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <h1 className="uppercase">FILTER BY Brand</h1>
-              <div className="text-sm my-5 h-60 overflow-y-scroll scrollbar_hidden cursor-all-scroll">
-                {brands.map((brand, index) => (
-                  <button
-                    key={index}
-                    className="flex justify-between items-center gap-3 py-1"
-                  >
-                    {brand.name}
-                    <p>({brand.count})</p>
-                  </button>
+              <div
+                className={`grid grid-cols-1 md:grid-cols-${dynamicGrid} justify-between items-center gap-5 my-5 w-full`}
+              >
+                {products?.map((product, index) => (
+                  <ShopCard key={index} product={product} />
                 ))}
               </div>
             </div>
-          </div>
-          <div className="md:col-span-5">
-            <div className="bg-[#f7f8fd] py-4 px-5 w-full rounded-md flex justify-between items-center">
-              <div className="flex justify-start items-center gap-5">
-                <TiThMenu
-                  onClick={() => handleDynamicGrid({ value: 1 })}
-                  className={`text-2xl text-gray-400 hover:text-gray-700 ${
-                    dynamicGrid == 1 ? "text-gray-700" : ""
-                  } duration-700`}
-                />
-
-                <RiGridFill
-                  onClick={() => handleDynamicGrid({ value: 3 })}
-                  className={`text-2xl text-gray-400 hover:text-gray-700 ${
-                    dynamicGrid == 3 ? "text-gray-700" : ""
-                  } duration-700`}
-                />
-                <TfiLayoutGrid4Alt
-                  onClick={() => handleDynamicGrid({ value: 4 })}
-                  className={`text-xl text-gray-400 hover:text-gray-700 ${
-                    dynamicGrid == 4 ? "text-gray-700" : ""
-                  } duration-700`}
-                />
-              </div>
-              <div className="flex items-center gap-3 ">
-                <div>
-                  <select
-                    name="sort"
-                    id="sort"
-                    className="focus:ring-0 focus:outline-0 px-3 "
-                  >
-                    <option className="" value="default">
-                      Short by latest
-                    </option>
-                    <option className="" value="pricehtl">
-                      Price: high to low
-                    </option>
-                    <option className="" value="pricelth">
-                      Price: low to high
-                    </option>
-                  </select>
-                </div>
-                <div>
-                  show
-                  <select
-                    name="show"
-                    id="show"
-                    className="focus:ring-0 focus:outline-0 px-3 border-none"
-                  >
-                    <option className="" value="default">
-                      9
-                    </option>
-                    <option className="" value="18">
-                      18
-                    </option>
-                    <option className="" value="27">
-                      27
-                    </option>
-                    <option className="" value="36">
-                      36
-                    </option>
-                  </select>
-                </div>
-              </div>
-            </div>
-            <div
-              className={`grid grid-cols-1 md:grid-cols-${dynamicGrid} justify-between items-center gap-5 my-5 w-full`}
-            >
-              {products?.products?.map((product, index) => (
-                <ShopCard key={index} product={product} />
-              ))}
-            </div>
-          </div>
-        </section>
-      </div>
+          </section>
+        </div>
+      ) : (
+        <Skeleton />
+      )}
     </main>
   );
 }
--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -3,8 +3,19 @@ import { fetchApi } from "@/utils/FetchApi";
 import Shop from "./Shop";
 
 export default async function Page() {
-  const products = await fetchApi("/product/getAllProducts", "GET");
-  console.log(products);
+  // const products = await fetchApi("/product/getAllProducts", "GET");
+  const products = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/product/getAllProducts`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  })
+    .then((res) => res.json())
+    .then((data) => data.products)
+    .catch((error) => console.error(error));
+
+  // console.log(products);
   return (
     <main>
       <Shop products={products} />
