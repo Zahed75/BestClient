5003ac15e267989f97390d7e7ab276010b525d8a
shaiadul <devshaiadul69@gmail.com>
2024-07-07 10:24:42 +0600

    leading remove no-store

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -10,7 +10,7 @@ export default async function Page() {
       headers: {
         "Content-Type": "application/json",
       },
-      // cache: "no-store",
+      cache: "no-store",
     }
   )
     .then((res) => res.json())
