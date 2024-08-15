2a3b915a421b40d61892a5ba89a68601fa7a75b0
shaiadul <devshaiadul69@gmail.com>
2024-08-15 11:20:49 +0600

    specification done

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,23 +1,46 @@
 import { fetchApi } from "@/utils/FetchApi";
 
-export default async function sitemap() {
-  const res = await fetchApi("/product/getProductByproductStatus", "GET");
-  const products = await res?.products;
+export default async function sitemap(req, res) {
+  const response = await fetchApi("/product/getProductByproductStatus", "GET");
+  const products = response?.products || [];
 
   const productUrls = products.map((product) => ({
     url: `${process.env.SITE_URL}/shop/${product.productSlug}`,
-    lastModified: new Date(product.updatedAt),
+    lastModified: new Date(product.updatedAt).toISOString(),
   }));
 
-  return [
+  const staticUrls = [
     {
       url: `${process.env.SITE_URL}`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
     {
       url: `${process.env.SITE_URL}/shop`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
     },
-    ...productUrls,
   ];
+
+  const allUrls = [...staticUrls, ...productUrls];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${allUrls
+        .map((url) => {
+          return `
+          <url>
+            <loc>${url.url}</loc>
+            <lastmod>${url.lastModified}</lastmod>
+          </url>
+        `;
+        })
+        .join("")}
+    </urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},  
+  };
 }
