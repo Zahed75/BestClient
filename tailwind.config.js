76b3444ebbaf3e8ddd4a94658219d4f1809987c1
shaiadul <devshaiadul69@gmail.com>
2024-08-31 10:10:40 +0600

    Shop: shop card change to product card

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
       padding: {
         DEFAULT: "1rem",
         sm: "2rem",
-        md: "3rem",
+        md: "1rem",
         lg: "4rem",
         xl: "5rem",
         "2xl": "6rem",
