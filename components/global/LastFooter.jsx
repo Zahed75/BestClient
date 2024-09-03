6540b35b5c02402827f9ff23d3f37799db6ff04f
MINHAJUL ISLAM <minhajul_islam1@hotmail.com>
2024-09-03 16:22:31 +0600

    Update Footer

--- components/global/LastFooter.jsx
+++ components/global/LastFooter.jsx
@@ -4,14 +4,14 @@ import sslImage from "@/public/images/ssl.png";
 export default function LastFooter() {
   return (
     <section className="container">
-      <div className="flex flex-col-reverse md:flex-row justify-between items-center py-10">
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center py-10 mb-5">
         <div>
           <span>
             © Best Electronics - All Rights Reserved. Payment Partner:
             SSLCOMMERZ
           </span>
         </div>
-        <div className="flex flex-col md:flex-row items-center gap-x-5">
+        <div className="flex flex-col lg:flex-row items-center gap-x-5">
           <span className="text-nowrap">Terms & Conditions</span>
           <Image src={sslImage} className="w-full" alt="SSL Commerz" />
         </div>
