69c7496ab4559670d2c43a5aa143f3175c18bcf1
shaiadul <devshaiadul69@gmail.com>
2024-09-10 10:36:57 +0600

    wishlistCard: card designe changed

--- components/productCard/wishlistCard.jsx
+++ components/productCard/wishlistCard.jsx
@@ -1,17 +1,15 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import Dynamo from "@/public/images/dynacool.jpg";
+import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 import { addToCart, updateQuantity } from "@/redux/slice/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromWishlist } from "@/redux/slice/wishlistSlice";
 
 export default function WishlistCard({ product }) {
-  const [cartCount, setCartCount] = useState(0);
-  const [productImage, setProductImage] = useState(
-    product?.productImage || Dynamo
-  );
+  const [isInCart, setIsInCart] = useState(false);
+  const [productImage, setProductImage] = useState(product?.productImage);
 
   const cart = useSelector((state) => state.cart.items);
   const dispatch = useDispatch();
@@ -19,6 +17,10 @@ export default function WishlistCard({ product }) {
   useEffect(() => {
     setProductImage(product?.productImage);
   }, [product]);
+
+  useEffect(() => {
+    setIsInCart(!!cart.find((item) => item._id === product?._id));
+  }, [cart, product?._id]);
 
   return (
     <div
@@ -56,7 +58,7 @@ export default function WishlistCard({ product }) {
             />
           </svg>
         </div>
-        <Link href={`/shop/${product?._id}`}>
+        <Link href={`/shop/${product?.productSlug}`}>
           <div className="object-cover min-h-[200px] flex justify-center overflow-hidden">
             <Image
               src={productImage}
@@ -67,13 +69,31 @@ export default function WishlistCard({ product }) {
             />
           </div>
         </Link>
+        <div className="mt-5 flex justify-start items-center">
+          <p
+            className={`${
+              product?.inventory?.stockStatus === "In Stock"
+                ? "text-[#70BE38]"
+                : "text-red-400"
+            } text-[11px] md:text-[12px] font-semibold ${
+              product?.inventory?.stockStatus === "In Stock"
+                ? "border border-[#70BE38]"
+                : "border border-red-400 bg-red-100"
+            } rounded-md px-2 md:px-3 md:py-1`}
+          >
+            {product?.inventory?.stockStatus}
+          </p>
+          {/* <span className="text-[#F16521] text-xs font-semibold ml-3 px-3 py-1 border border-[#F16521] rounded-md">
+              {product?.inventory?.inventoryStatus}
+            </span> */}
+        </div>
         <div className="mt-3">
-          <Link href={`/shop/${product?._id}`}>
-            <h4 className="text-[#202435] hover:text-[#F16521] duration-700 text-md font-semibold h-14">
+          <Link href={`/shop/${product?.productSlug}`}>
+            <h4 className="text-[#202435] hover:text-[#F16521] duration-700 text-[14px] md:text-[15px] font-semibold h-10 md:h-[45px] text-ellipsis overflow-hidden line-clamp-2">
               {product?.productName}
             </h4>
           </Link>
-          <div className="mt-5 text-slate-500 text-md">
+          <div className="mt-5 text-slate-500 text-[13px] md:text-[14px]">
             <div className=" ">
               Offer Price:{" "}
               <span className="font-semibold ml-1">
@@ -84,7 +104,7 @@ export default function WishlistCard({ product }) {
               M.R.P:
               <del className="ml-1">৳{product?.general?.regularPrice}</del>
             </div>
-            <div className="flex justify-start items-center">
+            <div className="flex justify-start items-center text-nowrap">
               Your Save:
               <div className="ml-1 flex justify-start items-center">
                 {product?.general?.salePrice ? (
@@ -108,61 +128,78 @@ export default function WishlistCard({ product }) {
               </div>
             </div>
           </div>
-          <div className="mt-5 flex justify-start items-center">
-            <p
-              className={`${
-                product?.inventory?.stockStatus === "In Stock"
-                  ? "text-[#70BE38]"
-                  : "text-red-400"
-              } text-xs font-semibold ${
-                product?.inventory?.stockStatus === "In Stock"
-                  ? "border border-[#70BE38]"
-                  : "border border-red-400 bg-red-100"
-              } rounded-md px-3 py-1`}
-            >
-              {product?.inventory?.stockStatus}
-            </p>
-            <span className="text-[#F16521] text-xs font-semibold ml-3 px-3 py-1 border border-[#F16521] rounded-md">
-              {product?.inventory?.inventoryStatus}
-            </span>
-          </div>
-          <div className="hidden group-hover:block duration-700 justify-between items-center mt-5 absolute bottom-0 right-0 w-full bg-white">
-            {Array.isArray(cart) &&
-            cart.find((item) => item._id === product?._id) ? (
-              <div className="bg-[#F16521] border rounded-full w-full flex justify-between items-center">
-                <button
-                  onClick={() => {
-                    dispatch(
-                      updateQuantity({ id: product?._id, quantity: -1 })
-                    );
-                  }}
-                  className="px-3 py-1 text-lg"
+
+          <div className="mt-5 w-full text-md">
+            <AnimatePresence mode="wait">
+              {isInCart ? (
+                <motion.div
+                  key="inCart"
+                  className="bg-[#FFCD00] rounded-full w-full flex justify-between items-center font-semibold"
+                  initial={{ width: "50%" }}
+                  animate={{ width: "100%" }}
+                  exit={{ width: "50%" }}
+                  transition={{ duration: 0.7 }}
                 >
-                  -
-                </button>
-                <button className="bg-white w-full py-1 text-lg">
-                  {cart.find((item) => item._id === product?._id)?.quantity}
-                </button>
-                <button
+                  <button
+                    onClick={() => {
+                      const currentQuantity = cart.find(
+                        (item) => item._id === product?._id
+                      )?.quantity;
+
+                      if (currentQuantity > 1) {
+                        dispatch(
+                          updateQuantity({ id: product?._id, quantity: -1 })
+                        );
+                      } else {
+                        setIsInCart(false);
+                        dispatch(
+                          updateQuantity({ id: product?._id, quantity: -1 })
+                        );
+                      }
+                    }}
+                    className="px-3 py-2"
+                  >
+                    -
+                  </button>
+                  <button className="w-full py-2">
+                    {cart.find((item) => item._id === product?._id)?.quantity}
+                  </button>
+                  <button
+                    onClick={() => {
+                      dispatch(
+                        updateQuantity({ id: product?._id, quantity: 1 })
+                      );
+                    }}
+                    className=""
+                  >
+                    <span className="bg-[#dbb51f] rounded-full w-2 h-2 px-3 py-2 mr-1 shadow-inner">
+                      +
+                    </span>
+                  </button>
+                </motion.div>
+              ) : (
+                <motion.button
+                  key="addToCart"
                   onClick={() => {
-                    dispatch(updateQuantity({ id: product?._id, quantity: 1 }));
+                    dispatch(addToCart(product));
+                    setIsInCart(true);
                   }}
-                  className="px-3 py-1 text-lg"
+                  className="bg-[#FFCD00] px-3 py-2 rounded-full w-full md:w-2/4 transition-all duration-500"
+                  initial={{ width: "50%" }}
+                  animate={{ width: "50%" }}
+                  exit={{ width: "50%" }}
+                  transition={{ duration: 0.7 }}
                 >
-                  +
-                </button>
-              </div>
-            ) : (
-              <button
-                onClick={() => {
-                  dispatch(addToCart(product));
-                  setCartCount(1);
-                }}
-                className="bg-[#FFCD00] px-3 py-2 rounded-full w-full"
-              >
-                Add to Cart
-              </button>
-            )}
+                  <motion.span
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.9, delay: 0.1 }}
+                  >
+                    Add to Cart
+                  </motion.span>
+                </motion.button>
+              )}
+            </AnimatePresence>
           </div>
         </div>
       </div>
